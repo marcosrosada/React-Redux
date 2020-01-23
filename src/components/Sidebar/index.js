4d1b013b615d660fbfad37ec6983e6b76788ad70
Marcos R F Rosada <mrfreitasrosada@europe.corp.altran.com>
2020-01-23 12:41:25 +0000

    CombineReducers

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
-function toggleLesson(module, lesson) {
-  return {
-    type: 'TOGGLE_LESSON',
-    module,
-    lesson
-  }
-}
+import * as CoursesActions from '../../store/actions/course';
 
 const Sidebar = ({ modules, dispatch }) => (
   <aside>
@@ -19,7 +13,7 @@ const Sidebar = ({ modules, dispatch }) => (
           {module.lessons.map(lesson => (
             <li key={lesson.id}>
               {lesson.title}
-              <button onClick={() => dispatch(toggleLesson(module, lesson))}>Selecionar</button>
+              <button onClick={() => dispatch(CoursesActions.toggleLesson(module, lesson))}>Selecionar</button>
             </li>
           ))}
         </ul>
